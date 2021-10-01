a61f73c543646dcb1e297d12c6f1c6e0c6e839eb
waffle52 <asuka9767@protonmail.com>
2021-10-01 11:07:36 -0700

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Kyle Campbell on 10/1/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/PacManScoreCheckerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PacManScoreCheckerApp.swift
+//  Shared
+//
+//  Created by Kyle Campbell on 10/1/21.
+//
+
+import SwiftUI
+
+@main
+struct PacManScoreCheckerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
